261dbd9b5895b0fe230a066b4909c0a278c6ea50
Anna Mendoza <annamendoza.curve@gmail.com>
2017-09-24 00:22:59 -0700

    updated the video template to be able to pull the properties off the snippet object and then render both the video and the title in the component

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -4,8 +4,33 @@ import React from 'react';
 // 	return <li>Video</li>
 // };
 
-const VideoListItem = (props) => {
-	return <li>Video</li>
+// ES5:
+// const VideoListItem = (function(props) {
+// 	const video = props.video;
+// return <li>Video</li>
+// };
+
+//
+
+// ES6: Since we know there's a video property
+// then we can simply pass it as a single argument
+// and return the element
+const VideoListItem = ({video}) => {
+	const imageUrl = video.snippet.thumbnails.default.url;
+  //console.log(video);
+	
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl} />
+				</div>
+				<div className="media-body">
+					<div className="media-heading">{video.snippet.title}</div>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default VideoListItem;
