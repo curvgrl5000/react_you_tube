f3141cec2a03adbe071c1297d371f3f9ab958142
Anna Mendoza <annamendoza.curve@gmail.com>
2017-09-23 23:08:38 -0700

    created a loop with map that takes an array and maps it into the videoListItem then renders it into the videoList

---
+++ src/components/video_list_item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// const VideoListItem = (function(props) {
+// 	return <li>Video</li>
+// };
+
+const VideoListItem = (props) => {
+	return <li>Video</li>
+};
+
+export default VideoListItem;
