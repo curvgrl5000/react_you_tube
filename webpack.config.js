f2712d50d4366770b05cb86ab43d6f09300e5af8
Anna Mendoza <annamendoza.curve@gmail.com>
2019-08-02 23:36:26 -0700

    updates for deployments

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
   ],
   output: {
     path: __dirname + '/build',
-    // publicPath: '/', // for dev
-    publicPath: '/react_you_tube', // for prod
+    publicPath: '/', // for dev
+    //publicPath: '/react_you_tube', // for prod
     filename: './build/bundle.js'
   },
   module: {
