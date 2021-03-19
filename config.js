22007cea6be878ea6de7f54083d98a6a48574188
Anna Mendoza <annamendoza.curve@gmail.com>
2021-03-18 19:27:49 -0700

    added another API key

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-const API_KEY = 'AIzaSyCkCPC67Tab6qIRGxV3dwgAYf7hcrOfqVM';
-//const API_KEY = 'AIzaSyAn06CRBrx5f6F9dFrCSRUrI9j_fpLcDJM'
+const API_KEY = 'AIzaSyAmXKYxDDGVivXV8zYt2uZc1pyx6QYKQwo';
+
+// const API_KEY = 'AIzaSyCkCPC67Tab6qIRGxV3dwgAYf7hcrOfqVM';
+// const API_KEY = 'AIzaSyAn06CRBrx5f6F9dFrCSRUrI9j_fpLcDJM'
 export default API_KEY;
 
 
